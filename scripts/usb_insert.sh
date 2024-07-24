820d59224669ef9f2efe20648784a6b16089777b
Shrrivathsa Mahesh <stillwater4500@gmail.com>
2024-07-24 21:54:29 +0530

    updated stuff

--- scripts/usb_insert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SOUND_FILE="/home/cheecho/.sounds/linuxmint/plug.oga"
-
-# Check if the sound file exists
-if [ -f "$SOUND_FILE" ]; then
-    pw-play "$SOUND_FILE"
-else
-    echo "Sound file not found: $SOUND_FILE"
-fi
